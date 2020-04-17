6c0f27767f07efd03ec1a658d36658d05835ffe5
Sundisttt <sundisttt@gmail.com>
2020-04-17 17:12:48 +0300

    Update passGenerator.py

--- passGenerator.py
+++ passGenerator.py
@@ -1,12 +1,23 @@
-
+import random 
 import string
-import random
-def randomString(stringLength):
-    """Generate a random string of 15 charcters"""
-    letters = string.ascii_letters
-    numbers = string.digits
-    imges = string.punctuation
-    alpha = letters + numbers + imges
-    return ''.join(random.choice(alpha) for i in range(stringLength))
 
-print ("Random String is ", randomString(15) )
+def passwordGenerator(N):
+	"""
+	Generate N range password including numbers, 
+	lowercases, uppercases and punctions
+	"""
+	password = ""
+	for i in range(0,N//4):
+		#In every iterate 4 chars gonna add so needed to divide by 4 
+		password += "".join(random.choice(string.ascii_lowercase)+
+			random.choice(string.ascii_uppercase)+
+			random.choice(string.digits)+
+			random.choice(string.punctuation))
+	print(password)
+	print(len(password))
+
+
+if __name__ == '__main__':
+	digitsNum = int(input("How many digits gonna be in password??"))
+	passwordGenerator(digitsNum)
+
